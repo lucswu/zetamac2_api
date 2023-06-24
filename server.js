1ecccb2fdffd997961f121f427f073bb0ab6e12c
Lucas Wu <lucaswuhuang@gmail.com>
2023-06-23 21:57:24 -0700

    added update score to mongodb functionality

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ const Room = mongoose.model("rooms", {
   roomName: String,
   roomId: String,
   ownerName: String,
+  inGame: Boolean,
   players: [
     {
       playerName: String,
@@ -57,6 +58,8 @@ app.post("/api/create-room", async (req, res) => {
     const room = new Room({
       roomName: roomName,
       roomId: UUID(0).uuid(),
+      ownerName: playerName,
+      inGame: false,
       players: [{ playerName: playerName, playerScore: 0 }],
     });
     await room.save();
@@ -67,6 +70,7 @@ app.post("/api/create-room", async (req, res) => {
       roomName: room.roomName,
       roomId: room.roomId,
       playerName: room.playerName,
+      url: `/game-room?roomName=${room.roomName}&playerName=${req.body.playerName}`,
     });
   } catch (error) {
     console.error("Error creating room:", error);
@@ -93,9 +97,6 @@ app.post("/api/join-room", async (req, res) => {
       return;
     }
 
-    // Generate player ID
-    // const playerId = UUID(1).uuid();
-
     // Add player name to database (Example: adding playerName to room.players array)
     room.players.push({ playerName, playerScore: 0 });
     await room.save();
@@ -105,12 +106,98 @@ app.post("/api/join-room", async (req, res) => {
       message: "Joined room successfully",
       roomName: room.roomName,
       playerName: room.playerName,
+      url: `/game-room?roomName=${room.roomName}&playerName=${req.body.playerName}`,
     });
   } catch (error) {
     console.error("Error joining room:", error);
     res.status(500).json({
       success: false,
       message: "Failed to join room",
+    });
+  }
+});
+
+app.get("/api/get-info", async (req, res) => {
+  try {
+    const { roomName } = req.body;
+    const room = await Room.findOne({ roomName });
+
+    if (!room) {
+      res.status(404).json({
+        success: false,
+        message: "Room not found",
+      });
+      return;
+    }
+    // Return room info
+    res.json({
+      success: true,
+      roomName: room.roomName,
+      roomId: room.roomId,
+      ownerName: room.ownerName,
+      inGame: room.inGame,
+      players: room.players,
+    });
+  } catch (error) {
+    console.error("Error getting room info:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to get room info",
+    });
+  }
+});
+
+app.post("/api/update-score", async (req, res) => {
+  try {
+    const { roomName, playerName, score } = req.body;
+
+    const room = await Room.findOneAndUpdate(
+      { roomName: roomName, "players.playerName": playerName },
+      { $set: { "players.$.playerScore": score } }
+    );
+    if (!room) {
+      res.status(404).json({
+        success: false,
+        message: "Room not found",
+      });
+      return;
+    }
+  } catch (error) {
+    console.error("Error updating score:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to update score",
+    });
+  }
+});
+
+app.post("/api/end-game", async (req, res) => {
+  try {
+    const { roomName } = req.body;
+    const room = await Room.findOneAndUpdate(
+      { roomName },
+      { $set: { inGame: false } }
+      // (err, result) => {
+      //   if (err) {
+      //     console.error("Error updating inGame:", error);
+      //   } else {
+      //     console.log("inGame updated successfully");
+      //   }
+      // }
+    );
+
+    if (!room) {
+      res.status(404).json({
+        success: false,
+        message: "Room not found",
+      });
+      return;
+    }
+  } catch (error) {
+    console.error("Error updating inGame:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to update inGame",
     });
   }
 });
